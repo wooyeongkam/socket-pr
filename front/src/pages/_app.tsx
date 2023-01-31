784993268a49f7844b5060caeef50d69350b1881
kamwooyeong <wooyeong.kam@avikus.ai>
2023-02-01 01:18:57 +0900

    install library

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
-if (process.env.NODE_ENV === "development") {
-  if (typeof window !== "undefined") {
-    const { worker } = require("../mocks/browser")
-    worker.start()
-  }
-}
+// if (process.env.NODE_ENV === "development") {
+//   if (typeof window !== "undefined") {
+//     const { worker } = require("../mocks/browser")
+//     worker.start()
+//   }
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
