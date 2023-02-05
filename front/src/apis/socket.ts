9af26ba7e05b9b043d692e705c5de0bdadc9a7c8
kamwooyeong <wooyeong.kam@avikus.ai>
2023-02-05 22:24:34 +0900

    frontend

--- front/src/apis/socket.ts
+++ front/src/apis/socket.ts
@@ -1,5 +1,5 @@
 import { Manager } from "socket.io-client"
 
-const manager = new Manager("http://localhost:3001")
+const manager = new Manager("http://localhost:8080")
 
-export const socket = manager.socket("/chat?username=KAM")
+export const socket = manager.socket("/badge")
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import type { NextPage } from "next"
 
 import { Bell, BellBadge } from "@/components/Bells"
@@ -7,20 +7,48 @@ import { socket } from "@/apis/socket"
 
 const Home: NextPage = () => {
   const [onAir, setOnAir] = useState(false)
-  const [connected, setConnected] = useState(false)
+  const [alerts, setAlerts] = useState<string[]>([])
 
   const handleClick = () => {
-    socket.on("connect", () => {
-      setConnected(true)
-    })
+    if (onAir) {
+      setOnAir(false)
+      socket.emit("alerts")
+      return
+    }
+
+    socket.off("alerts")
+    setAlerts([])
   }
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("badge connect")
+      socket.emit("new_alert")
+    })
+
+    socket.on("new_alert", (data: true) => {
+      console.log("new_alert", data)
+      if (!onAir) {
+        setOnAir(true)
+      }
+    })
+
+    socket.on("alerts", (data: string[]) => {
+      console.log("alerts", data)
+      setAlerts(data)
+    })
+  }, [])
 
   return (
     <div className={styles.box}>
       <button className={styles.button} onClick={handleClick}>
         {onAir ? <BellBadge /> : <Bell />}
       </button>
-      <p>{connected}</p>
+      <div className={styles.list}>
+        {alerts.map((alert, index) => (
+          <div key={alert.length + index}>{alert}</div>
+        ))}
+      </div>
     </div>
   )
 }
